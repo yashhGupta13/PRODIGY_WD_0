1cb15f6c5988fe4a26c9a374c14a11beb6a079ab
yashhGupta13 <143389399+yashhGupta13@users.noreply.github.com>
2024-05-15 19:07:56 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b30b7e6c53msh5810dd0d08cf185p136870jsn95d8f43df307",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e56f0cad1c025dba5d8178fe6a1c5d38"; // enter your key from openweather API
